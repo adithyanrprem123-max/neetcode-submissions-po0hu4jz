284e8cc48a0ed585205d2a8ac0ddaad451c00dd5
adithyanrprem123-max <adithyanrprem123@gmail.com>
2026-04-17 19:42:53 +0530

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,14 @@
+variable = 10
+print(type(variable))
+
+variable = 2.3
+print(type(variable))
+
+variable = True
+print(type(variable))
+
+variable = 'dfghd'
+print(type(variable))
+
+variable = [2,5]
+print(type(variable))
